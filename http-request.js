8763606adc2ce1cf851189815fcf79febcc011fc
Saria Mirza <sariamrza@gmail.com>
2021-09-04 16:10:57 +0300

    Release version 1.0.0

---
+++ http-request.js
@@ -0,0 +1,33 @@
+const httpRequest = (url, conf = {}) => {
+    
+	if (conf.method && conf.method.toUpperCase() === "GET" && conf.body) {
+		//GET request can't hold a body
+		delete conf.body;
+	}
+
+	return fetch(url, conf)
+		.then((respone) => {
+			//If the status code represents an error state 400-500 range, don't return the data
+			if (!respone.ok) {
+				//Read the error message and the status code
+				const statusCode = respone.status;
+				let errorMessage = "";
+				//Return the rejected promise to catch it
+				return respone.json().then((error) => {
+					//Some resonse dosen't have an error message, so create one
+					if (!error || (error && Object.keys(error).length === 0)) {
+						errorMessage = `Error from the client side, check the status code: ${statusCode}`;
+					} else {
+						errorMessage = `${error}, the status code: ${statusCode}`;
+					}
+					throw new Error(errorMessage);
+				});
+			}
+
+			//Return a promise with a snapshot of the data stream
+			return respone.json();
+		})
+		.catch((error) => {
+			throw new Error(error.message);
+		});
+};
